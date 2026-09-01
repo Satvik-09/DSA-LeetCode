50e1cb7ac07b1950af7be18876ccbd01393577a8
Satvik <ekakshgavali@gmail.com>
2026-09-01 23:09:14 +0530

    Time: 2 ms (34.86%) | Memory: 45.3 MB (78.62%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,32 +1,23 @@
 class Solution {
-       private void getperms(int[] nums,int idx,List<List<Integer>> res) {
-        if(idx == nums.length){
-            List<Integer> cur = new ArrayList<>();
-            for( int num : nums){
-            cur.add(num);
-            }
-            res.add(cur);
-            return; 
-        }
 
-        for(int i = idx; i<nums.length; i++){
-            swap(nums,i,idx);
-            getperms(nums,idx+1,res);
-            swap(nums,idx,i);
-        }
-       }     
-
-        private void swap(int[] nums,int idx, int i){
-            int temp = nums[idx];
-            nums[idx] = nums[i];
-            nums[i] = temp;
-        }
-   
-
-
-    public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        getperms(nums,0,res);
-        return res;
+ private void perms(int[] nums,List<Integer> cur,List<List<Integer>> res){
+    // Base case
+    if(cur.size() == nums.length){
+        res.add(new ArrayList<>(cur));
+        return;
     }
+    // to add each element and backtrack 
+    for(int i = 0; i<nums.length;i++){
+        if(cur.contains(nums[i])) continue;
+        cur.add(nums[i]); // choose
+        perms(nums,cur,res); // explore
+        cur.remove(cur.size()-1); // unchoose
+    }
+    
+ }
+    public List<List<Integer>> permute(int[] nums) {
+    List<List<Integer>> res = new ArrayList<>();
+    List<Integer> cur = new ArrayList<>();
+    perms(nums,new ArrayList<>(),res);
+    return res; }
 }
