22bdc808990ba37f75eb61d6b6662852849ce09b
Satvik <ekakshgavali@gmail.com>
2026-09-10 20:19:39 +0530

    Time: 1 ms (95.37%) | Memory: 43.7 MB (16.17%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,17 +1,20 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> res = new ArrayList<>();
-      
        
-       for(int row = 0; row<numRows; row++){
-         List<Integer> cur = new ArrayList<>();// it should get reset at every loo
-        long ans = 1;
-        cur.add((int)ans);
-          for(int c = 1; c<=row; c++){
-            ans = ans * (row-c+1)/c;
-            cur.add((int)ans);
-            
-          }res.add(new ArrayList<>(cur));
-       }  return res;
+          for(int row = 0; row < numRows; row++){
+              List<Integer> cur = new ArrayList<>();
+            for(int col = 0; col<=row; col++){
+                if(row == col || col == 0){
+                    cur.add(1);
+                }
+                else{
+                    List<Integer> prevrow = new ArrayList<>();
+                    prevrow = res.get(row-1);
+                    int value = prevrow.get(col-1) + prevrow.get(col);
+                    cur.add(value);
+                }
+            } res.add(cur);
+          } return res;
     }
 }
